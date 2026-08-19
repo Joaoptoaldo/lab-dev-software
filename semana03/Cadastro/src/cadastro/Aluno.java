67b042096669d7d7ed038779798e46e4d27924aa
Laboratório <laboratorio@ACAD.UFN.EDU.BR>
2026-08-19 12:18:38 -0300

    add projetos semana 3

---
+++ semana03/Cadastro/src/cadastro/Aluno.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cadastro;
+
+/**
+ *
+ * @author laboratorio
+ */
+public class Aluno {
+    private String nomeCompleto;
+    private String dataNascimento;
+    private String sexo;
+    private String matricula;
+    private String curso;
+    private String cpf;
+    private String enderecoCompleto;
+    private String estado;
+    private String telefone;
+
+    public Aluno(String nomeCompleto, String dataNascimento, String sexo, String matricula, 
+                 String curso, String cpf, String enderecoCompleto, String estado, String telefone) {
+        this.nomeCompleto = nomeCompleto;
+        this.dataNascimento = dataNascimento;
+        this.sexo = sexo;
+        this.matricula = matricula;
+        this.curso = curso;
+        this.cpf = cpf;
+        this.enderecoCompleto = enderecoCompleto;
+        this.estado = estado;
+        this.telefone = telefone;
+    }
+
+    @Override
+    public String toString() {
+        return nomeCompleto + ";" + 
+               dataNascimento + ";" + 
+               sexo + ";" + 
+               matricula + ";" + 
+               curso + ";" + 
+               cpf + ";" + 
+               enderecoCompleto + ";" + 
+               estado + ";" + 
+               telefone + "\n";
+    }
+}
